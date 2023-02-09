e9c8f593f40154f8f705dc5241929f8c53b1dbe3
riadhabdouli <abdouliriadh1919@gmail.com>
2023-02-09 10:07:47 +0100

    Ajoutez des fichiers projet.

---
+++ ProductAPI/Data/DBContextClass.cs
@@ -0,0 +1,22 @@
+﻿using ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductAPI.Data
+{
+    public class DbContextClass : DbContext 
+
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
